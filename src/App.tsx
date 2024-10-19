49f7c8ec25d82b6d8c023f9e47efb4c7d6f2dbd2
Albert Hoxha <dev.alberthoxha@gmail.com>
2024-10-19 14:57:15 +0200

    feat: install necessary packages for ui and server running

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomeView from "./views/HomeView";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeView />,
+  },
+]);
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
-      <p>React App</p>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   );
 }
 
